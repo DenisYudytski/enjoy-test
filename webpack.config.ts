bd67e44ae3791061f22f0418aadce184f0ec7ff6
Denis <hemosorbtion@gmail.com>
2026-02-15 15:44:18 +0700

    add flex mode webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -10,41 +10,52 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
-export default {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    clean: true,
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    plugins: [new TsconfigPathsPlugin()],
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
-      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+export default (env: any, argv: any) => {
+  const isDev = argv.mode === "development";
+
+  return {
+    mode: isDev ? "development" : "production",
+
+    entry: "./src/index.tsx",
+
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      publicPath: "/",
+      clean: true,
+    },
+
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+      plugins: [new TsconfigPathsPlugin()],
+    },
+
+    module: {
+      rules: [
+        { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
+        { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+      new webpack.DefinePlugin({
+        "process.env.API_BASE_URL": JSON.stringify(process.env.API_BASE_URL),
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new webpack.DefinePlugin({
-      "process.env.API_BASE_URL": JSON.stringify(process.env.API_BASE_URL),
-    }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    open: true,
-    client: {
-      overlay: {
-        errors: true,
-        warnings: false,
+
+    devServer: {
+      historyApiFallback: true,
+      port: 3000,
+      open: true,
+      client: {
+        overlay: {
+          errors: true,
+          warnings: false,
+        },
       },
     },
-  },
-  mode: "development",
+  };
 };
